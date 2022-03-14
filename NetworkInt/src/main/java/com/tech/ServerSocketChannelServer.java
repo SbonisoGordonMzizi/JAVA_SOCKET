da2dc10dabb7ecedca849ac7476753924477afc6
smzizi021 <smzizi021@student.wethinkcode.co.za>
2022-03-14 11:49:04 +0200

    serverAndClient

--- NetworkInt/src/main/java/com/tech/ServerSocketChannelServer.java
+++ NetworkInt/src/main/java/com/tech/ServerSocketChannelServer.java
@@ -1,5 +1,52 @@
 package  com.tech;
 
+import java.io.IOException;
+import java.net.Socket;
+import java.net.SocketAddress;
+import java.nio.ByteBuffer;
+import java.net.ServerSocket;
+import java.nio.channels.ServerSocketChannel;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+
+
 public class ServerSocketChannelServer {
+    public static void main(String[] args) throws IOException {
+        String message = "I AM MR SERVER HELLO THERE!!!!!!";
+
+        ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+        serverSocketChannel.configureBlocking(false);
+        ServerSocket socketChannel = serverSocketChannel.socket();
+        socketChannel.bind(new InetSocketAddress(6000));
+        String serverAddress = socketChannel.getLocalSocketAddress().toString();
+        System.out.print("STARTING SERVER "+serverAddress+" ");
+        byte[] data = (message+"  "+serverAddress).getBytes();
+        ByteBuffer dataBuffer = ByteBuffer.wrap(data);
+        while (true){
+            System.out.print(".");
+            SocketChannel sc = serverSocketChannel.accept();
+            if(sc != null){
+                System.out.println();
+                System.out.println("Recived connection from "+sc.socket().getRemoteSocketAddress());
+                if(sc.isConnected()) {
+                    System.out.println("COOOOOOOOOONECTED");
+                    dataBuffer.flip();
+                    sc.write(dataBuffer);
+                    sc.close();
+                }else{
+                    System.out.println("Not Connected");
+                }
+            }else{
+                try{
+                    Thread.sleep(1000);
+                }catch (InterruptedException ie){
+                    System.out.println(ie.getMessage());
+                }
+
+            }
+
+        }
+    }
 
 }
--- NetworkInt/src/main/java/com/tech/SocketChannelClient.java
+++ NetworkInt/src/main/java/com/tech/SocketChannelClient.java
@@ -1,4 +1,32 @@
 package com.tech;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import java.net.ServerSocket;
+
 public class SocketChannelClient {
+    public static void main(String[] args) throws IOException {
+        SocketChannel socketChannel = SocketChannel.open();
+        socketChannel.configureBlocking(false);
+        InetSocketAddress addr = new InetSocketAddress("127.0.0.1",6000);
+        socketChannel.connect(addr);
+
+        while (!socketChannel.finishConnect()){
+            System.out.println("waiting to finish connection");
+        }
+        if(socketChannel.isConnected()){
+            System.out.println("Connected To Server");
+        }
+        ByteBuffer buffer = ByteBuffer.allocateDirect(2048);
+
+        int h = socketChannel.read(buffer);
+        System.out.println(h);
+        h = socketChannel.read(buffer);
+        System.out.println(h);
+        h = socketChannel.read(buffer);
+        System.out.println(h);
+        socketChannel.close();
+    }
 }
